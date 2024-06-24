f7e534b687fd0a5f4da98aa063e6350d3dab86d8
Callum Kloos <callumkloos@gmail.com>
2024-06-24 21:32:00 +0100

    add classname support to icons

--- app/components/icons/twitter.tsx
+++ app/components/icons/twitter.tsx
@@ -1,11 +1,13 @@
-export const Twitter = () => {
+import clsx from "clsx";
+
+export const Twitter = ({className}: {className?: string}) => {
   return (
     <svg
       width="24"
       height="24"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      className="fill-foreground"
+      className={clsx(className, "fill-foreground")}
     >
       <path d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231l5.45-6.231Zm-1.161 17.52h1.833L7.045 4.126H5.078L17.044 19.77Z" />
     </svg>
