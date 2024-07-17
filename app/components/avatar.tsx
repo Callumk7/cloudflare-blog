5d6e8b0aef1209c177456b2f5dbbd6a95b01ef9d
Callum Kloos <callumkloos@gmail.com>
2024-07-17 16:21:57 +0100

    colour updates

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -5,7 +5,7 @@ export function Avatar() {
 	return (
 		<div className="overflow-hidden relative w-9/12 rounded-full aspect-square">
 			<img
-				src={`${S3_URL}/images/avatar-scaled.jpeg`}
+				src={`${S3_URL}/images/avatar-scaled.jpg`}
 				alt="portrait"
 				className="object-cover object-center w-full h-full"
 			/>
