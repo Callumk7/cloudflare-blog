b8112740ebb57e643c24183134305c0b7f4c97ec
Callum Kloos <callumkloos@gmail.com>
2024-07-18 16:00:23 +0100

    content and cv update

--- app/routes/_app.cv.tsx
+++ app/routes/_app.cv.tsx
@@ -1,8 +1,6 @@
 import { getAllProjectData } from "@/api/projects";
-import { Avatar } from "@/components/avatar";
 import { SkillsList } from "@/components/cv/skills-list";
 import { WorkExperience } from "@/components/cv/work-experience";
-import Portrait from "@/components/home/portrait";
 import { Card } from "@/components/layout/card";
 import { Container } from "@/components/layout/container";
 import { H2 } from "@/components/layout/headers";
@@ -33,6 +31,7 @@ const skills = [
 	"nextjs",
 	"product management",
 	"ux research and design",
+	"git",
 ];
 
 export default function CVRoute() {
@@ -42,12 +41,12 @@ export default function CVRoute() {
 		<Container width={"mobMax"}>
 			<Card flex>
 				<Title title="Callum Kloos" />
-				<H2>Web Developer and Product Designer</H2>
+				<H2>Web Developer</H2>
 				<p className="pb-7">
 					Self motivated and ambitious web developer who has built complex web
-					applications from the ground up with HTML, CSS, Typescript and React. Ex.
-					Product manager with four years of experience, I am seeking new opportunities in
-					a technical web development role.
+					applications from the ground up with HTML, CSS, Typescript and React. Previously
+					a product manager and UX researcher with four years of experience, I am seeking
+					new opportunities in a web development role.
 				</p>
 				<div className="flex justify-between">
 					<SocialLinks />
@@ -55,10 +54,6 @@ export default function CVRoute() {
 						<span className="mr-5">Prefer a PDF copy?</span> <DownloadCVButton />
 					</p>
 				</div>
-			</Card>
-			<Card>
-				<H2>Skills</H2>
-				<SkillsList skills={skills} />
 			</Card>
 			<div className="grid gap-9 lg:grid-cols-3">
 				<div className="flex flex-col col-span-2 gap-4">
@@ -69,19 +64,32 @@ export default function CVRoute() {
 						dateFrom={new Date("2019-07-01")}
 						dateTo={new Date("2020-09-01")}
 					>
-						<div>
-							Dedicated full-time hours to immerse myself in the craft of web development.
-							Built{" "}
-							<Link className="link" to={"/projects/playq-videogame-playlist-manager"}>
-								playQ
-							</Link>{" "}
-							to touch all aspects of the web development pipeline. Built{" "}
-							<Link className="link" to={"/projects/this-little-piggy-richmond"}>
-								This Little Piggy
-							</Link>{" "}
-							for a local animal rescue charity. Built a personal portfolio. During this
-							time I have become proficient with React, Remix, Serverless and serverful
-							deployment environments, api design and database design.
+						<div className="prose prose-invert">
+							Dedicated extensive hours into developing a strong proficiency in
+							JavaScript, TypeScript, HTML, CSS, and React. Developed multiple projects
+							including:
+							<ul>
+								<li>
+									<Link to={"/projects/my-development-portfolio"}>callumkloos.dev</Link>{" "}
+									(personal portfolio)
+								</li>
+								<li>
+									<Link to={"/projects/playq"}>playq.xyz</Link> and{" "}
+									<Link to={"/projects/game-master-tabletop-rpg-note-taking"}>
+										game-master
+									</Link>{" "}
+									(full-stack react projects)
+								</li>
+								<li>
+									<Link to={"/projects/this-little-piggy-richmond"}>
+										thislittlepiggyrichmond.co.uk
+									</Link>{" "}
+									(website in production for local animal rescue)
+								</li>
+							</ul>
+							Experienced in modern frameworks like Remix to utilise modern server-driven
+							rendering patterns (SSR, SSG), as well as experience designing SQL schemas
+							and backend systems to handle highly dynamic data.
 						</div>
 					</WorkExperience>
 					<WorkExperience
@@ -90,9 +98,12 @@ export default function CVRoute() {
 						dateFrom={new Date("2019-07-01")}
 						dateTo={new Date("2020-09-01")}
 					>
-						Worked freelance as a Product Designer on an angel funded game development
-						platform. I helped to shape the initial product strategy, and lay the initial
-						design foundations for user testing and future development.
+						Collaborated closely with the founders to plan and implement a strong user
+						research strategy for an innovative and disruptive product category. Gathered
+						and presented key data around target demographics, market landscape and
+						opportunities through key UX research skills and techniques including user
+						interviews, user story mapping, heuristic evaluation of existing products, and
+						development of key user personas.
 					</WorkExperience>
 					<WorkExperience
 						institution="Receipt Bank"
@@ -100,21 +111,23 @@ export default function CVRoute() {
 						dateFrom={new Date("2019-07-01")}
 						dateTo={new Date("2020-09-01")}
 					>
-						Product Manager at a fast paced, high growth company using artificial
+						Receipt Bank (now Dext) is a fast paced, high growth company using artificial
 						intelligence to disrupt the accounting space. Joined after series B funding
-						through to a successful series C and later acquisition. I used agile
-						methodologies, collaborated with a multi-disciplinary team, and managed the
-						inception, research, testing, and iteration of features and initiatives which
-						lead to the release of the companies first automated submission method,
-						automated documentation fetching and bank integration, and the initiation of
-						product led engagement and retention strategies.
+						and worked through to a successful Series C investment run. Using agile
+						methodologies I collaborated with a multi-disciplinary team through inception,
+						research, testing, and iteration to introduce the company's first automated
+						submission method.
 					</WorkExperience>
-				</div>
-				<div className="col-span-2 space-y-3 w-full lg:col-span-1">
 					<H2>Projects</H2>
 					{projects.map((project) => (
 						<CVProjectCard project={project} key={project.slug} />
 					))}
+				</div>
+				<div className="col-span-2 space-y-3 w-full lg:col-span-1">
+					<Card>
+						<H2>Professional Skills</H2>
+						<SkillsList skills={skills} />
+					</Card>
 					<Separator />
 					<H2>Education</H2>
 					<WorkExperience
@@ -138,7 +151,7 @@ export default function CVRoute() {
 function DownloadCVButton() {
 	return (
 		<a
-			href="/files/callum-kloos-cv-2024.pdf"
+			href="https://ck-portfolio-images.s3.eu-west-2.amazonaws.com/files/callum-kloos-cv-2024.pdf"
 			target="_blank"
 			rel="noreferrer"
 			className="p-2 mr-6 rounded-md transition-colors duration-100 ease-in-out text-bold bg-primary-3 text-background hover:bg-primary-1 active:bg-primary-2"
