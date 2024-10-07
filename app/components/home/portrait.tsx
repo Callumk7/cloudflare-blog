56e61e7b4769aa58322dbec4fdc31f33352d8a46
Callum Kloos <Callumkloos@gmail.com>
2024-10-07 16:17:21 +0100

    fix positioning

--- app/components/home/portrait.tsx
+++ app/components/home/portrait.tsx
@@ -3,7 +3,7 @@ import { Title } from "../layout/title";
 
 export default function Portrait() {
   return (
-    <div className="flex flex-col gap-y-9 justify-center place-items-center">
+    <div className="flex flex-col pt-10 gap-y-9 place-items-center">
       <Title title="Callum Kloos" centered />
       <Avatar />
     </div>
