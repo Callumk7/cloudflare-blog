0840992ab7b2ba2f4192e552be048bda7013b87f
Callum Kloos <Callumkloos@gmail.com>
2024-10-21 13:32:39 +0100

    update text size

--- app/components/posts/post-body.tsx
+++ app/components/posts/post-body.tsx
@@ -6,7 +6,7 @@ export function PostBody({ content }: PostBodyProps) {
   return (
     <div className="mx-auto max-w-3xl">
       <div
-        className="max-w-none prose prose-lg prose-invert prose-h1:font-syne prose-h1:border-b prose-h1:border-foreground/20 prose-h1:text-3xl prose-h1:pt-6 prose-h1:pb-4 prose-h1:text-primary-1 prose-h2:font-syne prose-h2:font-bold prose-h2:text-primary-1 prose-h2:text-xl prose-h3:font-syne prose-h3:font-semibold prose-h3:text-xl prose-strong:text-primary-1 prose-code:font-mono prose-code:text-base"
+        className="max-w-none prose prose-lg lg:prose-base prose-invert prose-h1:font-syne prose-h1:border-b prose-h1:border-foreground/20 prose-h1:text-3xl prose-h1:pt-6 prose-h1:pb-4 prose-h1:text-primary-1 prose-h2:font-syne prose-h2:font-bold prose-h2:text-primary-1 prose-h2:text-xl prose-h3:font-syne prose-h3:font-semibold prose-h3:text-xl prose-strong:text-primary-1 prose-code:font-mono prose-code:text-base"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
