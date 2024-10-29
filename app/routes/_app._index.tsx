430aba08955ada3f0ae9c3881ad289b76ddd8091
Callum Kloos <Callumkloos@gmail.com>
2024-10-29 11:19:53 +0000

    remove blog posts form front-page

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -1,28 +1,25 @@
-import { getAllPostData } from "@/api/posts";
 import { getAllProjectData } from "@/api/projects";
 import Portrait from "@/components/home/portrait";
-import PostList from "@/components/home/post-list";
 import { Container } from "@/components/layout/container";
 import { H2 } from "@/components/layout/headers";
-import { Separator } from "@/components/layout/separator";
 import { ProjectList } from "@/components/projects/project-list";
 import { LoaderFunctionArgs, json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
-  const posts = await getAllPostData(context);
   const allProjects = await getAllProjectData(context);
   const completedProjects = allProjects.filter((project) => !project.wip);
-  const recentPosts = posts.slice(0, 3);
-  return json({ recentPosts, completedProjects });
+  return json({ completedProjects });
 };
 
 export default function AppIndex() {
-  const { recentPosts, completedProjects } = useLoaderData<typeof loader>();
+  const { completedProjects } = useLoaderData<typeof loader>();
   return (
     <Container className="my-10" width={"mobMax"}>
       <div className="gap-x-6 lg:grid lg:grid-cols-2">
-        <Portrait />
+        <div className="lg:sticky lg:top-[64px] lg:max-h-[calc(100vh-64px)]">
+          <Portrait />
+        </div>
         <div>
           <H2>Projects</H2>
           <p className="mb-8">
@@ -31,9 +28,6 @@ export default function AppIndex() {
           <ProjectList projects={completedProjects} />
         </div>
       </div>
-      <Separator className="my-8" />
-      <H2>Recent Posts</H2>
-      <PostList posts={recentPosts} />
     </Container>
   );
 }
