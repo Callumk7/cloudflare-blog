5f0b201cc413bd98a4a31bbc6df0be220249afa7
Callum Kloos <Callumkloos@gmail.com>
2024-10-30 10:26:42 +0000

    test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+	plugins: [remixCloudflareDevProxy(), !process.env.VITEST && remix(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./tests/setup.ts",
+		exclude: [...configDefaults.exclude],
+	},
 });
